168f15ef4a6d0d240a56969932d73eb0877e81cc
Joshua Hong <hong0008hk@yahoo.com.hk>
2013-09-06 01:15:32 +0800

    Create OS3DUI.java
    OS3DUI

---
+++ OS3DUI.java
@@ -0,0 +1,13 @@
+package os3dui.originalsourcecompany.os3dui;
+
+import java.util.Vector;
+
+public class OS3DUI {
+
+	public float X_Coordinate;
+
+	public float Y_Coordinate;
+
+	public float Z_Coordinate;
+
+}
